742548e0dbca452d1bf1ef7a1abd4693db45b11d
Dobrovolskiy Danil <falgik2@gmail.com>
2021-12-16 22:20:24 +0200

    Добавьте файлы проекта.

---
+++ laba_4.cpp
@@ -0,0 +1,175 @@
+﻿// lab_4.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "laba_4.h"
+#include "MMSystem.h"
+#pragma comment(lib, "Winmm.lib")
+
+
+#define MAX_LOADSTRING 100
+#define IMAGECOUNT 47 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LABA4, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LABA4));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LABA4));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = 0;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+
+
+
+HBITMAP hBitmapimg[IMAGECOUNT];
+BITMAP bmImg;
+HDC memBit;
+std::wstring buf;
+int curentFrame;
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+
+        buf = _T("var2\\0.bmp");//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        for (int i = 0; i < IMAGECOUNT; i++)
+        {
+            while (buf.size() > 5)
+                buf.pop_back();
+            buf += std::to_wstring(i);
+            buf += _T(".bmp");
+            hBitmapimg[i] = (HBITMAP)LoadImage(NULL, buf.c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+        }
+
+        SetTimer(hWnd, 1, 50, 0);
+        PlaySound(TEXT("1.wav"), NULL, SND_ASYNC | SND_SENTRY);
+        GetObject(hBitmapimg[0], sizeof(bmImg), &bmImg);
+
+        SetWindowPos(hWnd, hWnd, 0, 0, bmImg.bmWidth + 15, bmImg.bmHeight + 60, SWP_NOZORDER | SWP_NOMOVE);
+        break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        memBit = CreateCompatibleDC(hdc);
+
+        GetObject(hBitmapimg[curentFrame], sizeof(bmImg), &bmImg);
+        SelectObject(memBit, hBitmapimg[curentFrame]);
+
+        BitBlt(hdc, 0, 0, bmImg.bmWidth, bmImg.bmHeight, memBit, 0, 0, SRCCOPY);
+
+        DeleteObject(memBit);
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    case WM_TIMER:
+        curentFrame++;
+        if (curentFrame == IMAGECOUNT)
+            curentFrame = 0;
+
+        InvalidateRect(hWnd, 0, 0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
